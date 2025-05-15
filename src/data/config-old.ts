2613c7fac47f6fe65e71ed717f261af3ff8f19c8
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2025-05-15 21:26:52 +0530

    refactor: add content

--- src/data/config-old.ts
+++ src/data/config-old.ts
@@ -1,58 +1,58 @@
 export const BIRTHDAY_PERSON = 'Kanishka';
 
-export const YOUTUBE_MUSIC_URL = 'https://youtu.be/o_1aF54DO60'; // Young and Beautiful by Lana Del Rey
+export const YOUTUBE_MUSIC_URL = 'https://www.youtube.com/embed/Bag1gUxuU0g'; // Young and Beautiful by Lana Del Rey
 
 export const HERO_PHOTO = {
-  url: '/k001.jpeg',
+  url: '/k1.jpeg',
   caption: 'Happy Birthday to notkanishk! * meow *',
 };
 
 export const COLLAGE_PHOTOS = [
   {
     id: 1,
-    url: '/k002.jpeg',
+    url: 'https://images.pexels.com/photos/9374426/pexels-photo-9374426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Remember that incredible sunset?',
     rotation: '-15deg',
   },
   {
     id: 2,
-    url: '/k003.jpeg',
+    url: 'https://images.pexels.com/photos/5668473/pexels-photo-5668473.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'City adventures with you are the best!',
     rotation: '8deg',
   },
   {
     id: 3,
-    url: '/k004.jpeg',
+    url: 'https://images.pexels.com/photos/6170435/pexels-photo-6170435.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Coffee dates and deep talks',
     rotation: '-5deg',
   },
   {
     id: 4,
-    url: '/k005.jpeg',
+    url: 'https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Beach days forever',
     rotation: '12deg',
   },
   {
     id: 5,
-    url: '/k006.jpeg',
+    url: 'https://images.pexels.com/photos/3361739/pexels-photo-3361739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'That time we got lost in the forest',
     rotation: '-10deg',
   },
   {
     id: 6,
-    url: '/k007.jpeg',
+    url: 'https://images.pexels.com/photos/3278215/pexels-photo-3278215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Concert nights and city lights',
     rotation: '7deg',
   },
   {
     id: 7,
-    url: '/k008.jpeg',
+    url: 'https://images.pexels.com/photos/2253643/pexels-photo-2253643.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Sunrise hikes are our thing',
     rotation: '-8deg',
   },
   {
     id: 8,
-    url: '/k009.jpeg',
+    url: 'https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Pizza nights and movie marathons',
     rotation: '10deg',
   },
@@ -61,19 +61,19 @@ export const COLLAGE_PHOTOS = [
 export const TIMELINE_PHOTOS = [
   {
     id: 1,
-    url: '/k012.jpeg',
+    url: 'https://images.pexels.com/photos/936048/pexels-photo-936048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: 'Where it all began - 5 years ago',
     year: '2019',
   },
   {
     id: 2,
-    url: '/k010.jpeg',
+    url: 'https://images.pexels.com/photos/1701209/pexels-photo-1701209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: "Look how far we've come",
     year: '2022',
   },
   {
     id: 3,
-    url: '/k011.jpeg',
+    url: 'https://images.pexels.com/photos/5699177/pexels-photo-5699177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     caption: "Here's to many more years!",
     year: '2025',
   },
@@ -92,4 +92,3 @@ export const PARTY_SOUNDS = {
   birthday: '/sounds/happy-birthday.mp3',
   click: '/sounds/click.mp3',
 };
-
